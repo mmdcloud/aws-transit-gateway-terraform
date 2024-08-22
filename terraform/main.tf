e71b756dd3b6409a06849defc8c3fa6a8b107e00
mmdcloud <148881191+mmdcloud@users.noreply.github.com>
2024-08-22 08:25:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -256,6 +256,22 @@ resource "aws_security_group" "vpc3_sg" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
 data "aws_key_pair" "key_pair" {
   key_name = "surajm"
 }
