baa576bb531995cfd9750bd5533598b0587b8a10
mmdcloud <madmaxcloudpractice@gmail.com>
2025-07-18 09:22:01 +0530

    update: renamed outputs in tf and added private ip in outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,23 @@
-output "instance1_ip" {
+output "instance1_public_ip" {
   value = module.instance1.public_ip
 }
 
-output "instance2_ip" {
+output "instance2_public_ip" {
   value = module.instance2.public_ip
 }
 
-output "instance3_ip" {
+output "instance3_public_ip" {
   value = module.instance3.public_ip
+}
+
+output "instance1_private_ip" {
+  value = module.instance1.private_ip
+}
+
+output "instance2_private_ip" {
+  value = module.instance2.private_ip
+}
+
+output "instance3_private_ip" {
+  value = module.instance3.private_ip
 }
